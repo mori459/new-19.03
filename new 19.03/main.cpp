acfd12cbec8ac60e1ccfaf80ff0afb8d990dbbb1
mori459 <mobcoding459@gmail.com>
2024-03-19 16:55:13 +0500

    Добавьте файлы проекта.

---
+++ new 19.03/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <windows.h>
+
+int main()
+{
+	int arr[10];
+	for (int i = 0; i < 10; i++)
+	{
+		std::cin >> arr[i];
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		std::cout << arr[i] << "|";
+	}
+
+}

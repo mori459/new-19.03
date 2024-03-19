c93a5465f2f6b97cc1ffb6894fd1a4eadfc4b585
mori459 <mobcoding459@gmail.com>
2024-03-19 17:16:10 +0500

    ex 2

--- new 19.03/main.cpp
+++ new 19.03/main.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-	int arr[10];
+	/*int arr[10];
 	for (int i = 0; i < 10; i++)
 	{
 		std::cin >> arr[i];
@@ -11,6 +11,18 @@ int main()
 	for (int i = 0; i < 10; i++)
 	{
 		std::cout << arr[i] << "|";
+	}*/
+
+	const int size = 5;
+	int arr[size]{ -23, -11, 9, -18, 25 };
+	int result = 0;
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i] < 0)
+		{
+			result += arr[i];
+		}
 	}
+
 
 }
